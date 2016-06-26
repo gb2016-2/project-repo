95099c09def854e0db0955274fef33142bc8cc7a
vlfedotov <fedotov.vu@gmail.com>
2016-06-26 09:47:21 +0300

    Паук по именам сделан

--- crawler/python/name_crawler.py
+++ crawler/python/name_crawler.py
@@ -8,8 +8,9 @@ from collections import namedtuple
 class NameCrawler(object):
     def __init__(self, user, db):
         self._sites = {}
-        self._persons = []
+        self._persons = {}
         self._pages = []
+        self._names = {}
         self._config = {'user': user,
                         'database': db,
                         'charset': 'utf8'}
@@ -29,7 +30,18 @@ class NameCrawler(object):
             cnx.close()
 
     def add_person(self, person):
-        self._persons.append(person)
+        if person not in self._persons:
+            cnx = pymysql.connect(**self._config)
+            cur = cnx.cursor()
+
+            cur.execute("SELECT id FROM persons WHERE name='" + person + "';")
+            for row in cur:
+                person_id = row[0]
+                if person_id:
+                    self._persons[person] = person_id
+
+            cur.close()
+            cnx.close()
 
     def get_pages(self):
         Page = namedtuple('Page', ['page_id', 'url', 'site_id'])
@@ -44,6 +56,21 @@ class NameCrawler(object):
             for row in cur:
                 page = Page(*row[:3])
                 self._pages.append(page)
+
+        cur.close()
+        cnx.close()
+
+    def get_names(self):
+        cnx = pymysql.connect(**self._config)
+        cur = cnx.cursor()
+
+        for person, person_id in self._persons.items():
+            self._names[person] = []
+            cur.execute("SELECT name \
+                         FROM keywords \
+                         WHERE person_id='" + str(person_id) + "';")
+            for row in cur:
+                self._names[person].append(row[0])
 
         cur.close()
         cnx.close()
@@ -67,15 +94,16 @@ class NameCrawler(object):
             texts_by_page[page.url] = visible_texts
 
         self._count = {}
-        for pers in self._persons:
+        for pers in self._persons.keys():
             self._count[pers] = {}
         for page, texts in texts_by_page.items():
             for pers in self._count.keys():
                 self._count[pers][page] = 0
             for text in texts:
-                for person in self._count.keys():
-                    match = re.findall(person, text, flags=re.IGNORECASE)
-                    self._count[person][page] += len(match)
+                for person in self._persons.keys():
+                    for word in self._names[person]:
+                        match = re.findall(word, text, flags=re.IGNORECASE)
+                        self._count[person][page] += len(match)
 
     def print_results(self):
         for pers, pages in self._count.items():
@@ -90,28 +118,45 @@ class NameCrawler(object):
         cnx = pymysql.connect(**self._config, autocommit=True)
         cur = cnx.cursor()
         for page in self._pages:
-            for pers in self._persons:
-                sql = "INSERT \
-                       INTO person_page_rank(person_id, page_id, rank) \
-                       VALUES \
-                       (" + str(1) + ", " + str(page.page_id) + ", " \
-                          + str(self._count[pers][page.url]) + ");"
-                cur.execute(sql)
+            for pers, id in self._persons.items():
+                select_test = False
+                sql_test = "SELECT * \
+                            FROM person_page_rank \
+                            WHERE (person_id='" + str(id) + "') \
+                            AND (page_id='" + str(page.page_id) + "');"
+                cur.execute(sql_test)
+                for row in cur:
+                    select_test = True
+                if select_test:
+                    sql_update = "UPDATE person_page_rank \
+                                  SET \
+                                  rank='" + str(self._count[pers][page.url]) + "' \
+                                  WHERE (person_id='" + str(id) + "') \
+                                  AND (page_id='" + str(page.page_id) + "');"
+                    cur.execute(sql_update)
+                else:
+                    sql = "INSERT \
+                           INTO person_page_rank(person_id, page_id, rank) \
+                           VALUES \
+                           (" + str(id) + ", " + str(page.page_id) + ", " \
+                              + str(self._count[pers][page.url]) + ");"
+                    cur.execute(sql)
         cur.close()
         cnx.close()
 
 if __name__ == '__main__':
-    putin = 'путин'
-    zhir = 'жириновск'
-
     crawler = NameCrawler('bla-user', 'tmp')
     crawler.add_site('lenta.ru')
     crawler.get_pages()
     print(crawler._sites)
     print(crawler._pages)
 
-    crawler.add_person(putin)
-    crawler.add_person(zhir)
+    crawler.add_person('Путин')
+    crawler.add_person('Жириновский')
+    crawler.get_names()
+    print(crawler._persons)
+    print(crawler._names)
+
     crawler.count_persons()
     crawler.print_results()
     crawler.write_to_bd()
