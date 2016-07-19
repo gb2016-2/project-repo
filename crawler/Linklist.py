e4a6ac7f4194b1818720a6b5f8ba1449fdfeeb07
hyperios <portamundos@gmail.com>
2016-07-19 14:53:44 +0300

    приведен в порядок код, добавлены доки убраны тестовые файлы

--- crawler/Linklist.py
+++ crawler/Linklist.py
@@ -1,33 +1,37 @@
-from requests import get as getData
-import urllib.request
 import re
 import codecs
-from bs4 import BeautifulSoup as Soup
-import gzip
-from time import gmtime, strftime
-from datetime import datetime
 import sys
 import threading
 import time
+import urllib.request
+from time import gmtime, strftime
+from datetime import datetime
 
-class Linklist(): 
-    def __init__(self, ID, url_site): # check the start of the original string on 'http'
-        if 'http' in url_site:
+from requests import get as getData
+from bs4 import BeautifulSoup as Soup
+import gzip
+
+
+class Linklist():
+    
+    def __init__(self, ID, url_site):
+        #check the start of the original string on 'http'
+        if 'http' in url_site: 
             self.url_site = url_site
         else:
             self.url_site = 'http://' + self.url_site # add a protocol 
         self.ID = ID
+        
 
     def url_chek(self): # available of a page
         try:
             ufile = urllib.request.urlopen(self.url_site)
             url_soup = Soup(ufile , "html.parser")
+            
             if '404' in url_soup.findAll(name='title'):
                 return False
             else:
-                return True
-                
-                      
+                return True             
         except urllib.error.HTTPError:
             print('urllib.error.HTTPError')
             return False
@@ -50,31 +54,26 @@ class Linklist():
                 url_soup = Soup(url_string , "html.parser")
                 THR = [] 
                 
-
-                
                 for i in url_soup.findAll(name='url'):
                     url = ''.join(re.findall(r'<.*?>(http.*?)<.*?>', str(i)))
-                    lastmod = ''.join(re.findall(r'<lastmod>(http.*?)</lastmod>', str(i)))
+                    refind = re.findall(r'<lastmod>(http.*?)</lastmod>',str(i))
+                    lastmod = ''.join(refind)
                     
                     x = threading.Thread(target=tread_add_list, args=(url,))
                     x.start()
                     THR.append(x)
                     
-
                 for t in THR:
                     t.join()
-                print( str(len(url_soup.findAll(name='url'))) + 'added data tuple in' + str(len(List)))  
+                print( str(len(url_soup.findAll(name='url'))) \
+                       + 'added data tuple in' + str(len(List)))  
             except:
                 print(str(ufile) + ' not a gzipped file')
                 pass  
         
         if Linklist.url_chek(self):
             ufile = urllib.request.urlopen(self.url_site)
-            extr(ufile,List)
-            
-            
-            
-                
+            extr(ufile,List)       
         else:
             pass
         
@@ -82,12 +81,14 @@ class Linklist():
         return List
         
 def main():
-    z = Linklist('1','http://sitemap.xml.gz')
+    z = Linklist('1','http://lenta.ru/news/sitemap.xml.gz')
     y = z.extract()
     file = open('test_log.txt', 'a')
+    
     for x in y:
         file.write(str(x) + '\n')
-     
+    print('File whith url white')
+    file.close()
 if __name__ == '__main__':
     print('Its testing run. Enter to start')
     x = input()
--- crawler/run.py
+++ crawler/run.py
@@ -1,79 +1,79 @@
-from requests import get as getData
+"""The program is designed to parse and
+extract data from the sitemap. Here it
+is the main control unit, coordinating
+act which all the others."""
+
+import threading
+import time
 import re
 import codecs
+from requests import get as getData
 from bs4 import BeautifulSoup as Soup
 from Linklist import Linklist
 from Append_DB import Write_DB
-import threading
-import time
 
-def url_chek(url_site): # available of a page #добавить проверку на специфичный xml
+
+
+def url_chek(url_site): # available of a page 
         try:
             ufile = getData(url_site)
             if str(ufile) == '<Response [404]>':
-                print('404')
+                print('404 not available data \n')
                 return False
             elif str(ufile) == '<Response [200]>':
-                print('200')
+                print('200 data available \n')
                 return True
         except TypeError:
             return False     
         except:
-            print('404')
+            print('404 not available url \n')
             return False
-        
 
 
 def append_DB(id_parent, url_site):
-
-        print('started') 
         
+    def run_search(id_parent, url_site):
+        List_Urls = []
+        THR = []
         
+        ufile = getData(url_site).content #get conten of data file
+        url_soup = Soup( str(ufile) , "html.parser") # parse the structur
+         
+        for i in url_soup.findAll(name='loc'): #find all urls
+            url = ''.join(re.findall(r'<.*?>(http.*?)<.*?>', str(i))) #get http
+                
+            if 'sitemap' in url: #check template entry
+                print('Finde sitemap url \n')      
+                print(url)
+                x = Linklist(id_parent, url)
+                ex = threading.Thread(target=List_Urls.extend, \
+                                      args=(x.extract(),))
+                ex.start()
+                THR.append(ex) 
+            else:
+                print('Url is not a sitemap url \n')
+                pass
+                        
+        for t in THR: # waiting for the completion of all the processes
+            t.join()
 
-        def run_search(id_parent, url_site):
-                ufile = getData(url_site).content #get conten of data file
-                url_soup = Soup( str(ufile) , "html.parser") # parse the structur
-                List_Urls = []
-                THR = [] 
-                for i in url_soup.findAll(name='loc'): #find all urls
-                        url = ''.join(re.findall(r'<.*?>(http.*?)<.*?>', str(i))) #get http
-            
+        print(len(List_Urls))
+        x = Write_DB(List_Urls)
+        x.write_db()
                 
-                        if 'sitemap' in url: #check template entry
-                                print('good') 
-                                
-                                
-                                
-                                print(url)
-                                x = Linklist(id_parent, url)
-                                ex = threading.Thread(target=List_Urls.extend, args=(x.extract(),))
-                                ex.start()
-                                THR.append(ex)
-                                
-                        else:
-                                print('fucking shit')
-                                pass
-                for t in THR:
-                        t.join()
+    while url_chek(url_site) == False: # link availability check
+        time.sleep(50)
+    else:
+        print('Parsing started') 
+        run_search(id_parent, url_site)
+        pass
+                
+    print('The program ended successfully ... probably')
 
-                
-                
         
-                print(len(List_Urls))
-                x = Write_DB(List_Urls)
-                x.write_db()
-                
-        while url_chek(url_site) == False: # check links
-                time.sleep(50)
-        else:
-               run_search(id_parent, url_site)
-               pass
-                
-        print('All good')
 if __name__ == '__main__':
    while 1:
         print('Search started')
         append_DB('1' , 'http://lenta.ru/sitemap.xml')
-        print('Wait new day for update')
+        print('Wait new day for update \n')
         sleep(86400)
-        
